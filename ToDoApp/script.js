e96e4af6e0fcc5e7b682706746fdcad709c186cd
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-01-29 13:26:51 -0600

    can delete items usually - there's a problem with keys in some cases but keys will be handled by the backend and so this should be resolved

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -3,7 +3,7 @@ var text = '{ "items" : [ { "key": 0, "text": "Test", "complete": false }, { "ke
 var items = JSON.parse(text).items;
 
 document.addEventListener("DOMContentLoaded", function(event) {
-   loadList(items);
+   loadList();
 });
 
 
@@ -11,26 +11,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function addItem(){
 	//read the box, create new item, and append it to the list
 	var newItemText = document.getElementById("textbox").value;
-	var newItem = { "key": items.length, "text": newItemText, "complete": false };
+	var newItem = { "key": items[items.length - 1].key + 1, "text": newItemText, "complete": false };
 	items.push(newItem);
 	//reload the list and clear the input value
-	loadList(items);
+	loadList();
 	document.getElementById("textbox").value = "";
 }
 
-function loadList(items){
+function loadList(){
 	var list = document.getElementById("todoList");
 	list.innerHTML = "";
 	items.forEach(function(item){
+		//create list item
 		var element = document.createElement('li');
 		element.id = item.key;
+
+		//create checkbox
 		var checkbox = document.createElement('input');
 		checkbox.type = "checkbox";
-		checkbox.id = "checkbox";
+		checkbox.id = "checkbox" + item.key;
 		checkbox.checked = item.complete;
 		checkbox.addEventListener('change', changeState);
+
+		if (checkbox.checked === true){
+			var deleteButton = document.createElement('input');
+			deleteButton.type = "button";
+			deleteButton.id = "deleteButton" + item.key;
+			deleteButton.value = "X";
+			deleteButton.addEventListener("click", deleteTask);
+			element.appendChild(deleteButton);
+
+		}
+
+		//add all items to list item
 		element.appendChild(checkbox);
 		element.appendChild(document.createTextNode(item.text));
+
 		list.append(element);
 	});
 }
@@ -39,5 +55,10 @@ function changeState(evt){
 	var key = evt.target.parentElement.id;
 	var value = evt.target.checked;
 	items[key].complete = value;
-	console.log(items);
+	loadList();
+}
+
+function deleteTask(evt){
+	items.splice(evt.target.parentElement.id, 1);
+	loadList();
 }
